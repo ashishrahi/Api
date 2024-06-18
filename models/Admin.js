b77bbf4471b99449d875a58796be3d6102a38f22
ashishrahi <148324855+ashishrahi@users.noreply.github.com>
2024-06-18 13:33:58 +0530

    Api

--- models/Admin.js
+++ models/Admin.js
@@ -3,16 +3,13 @@ const {mongoose,Schema} = require('mongoose');
     username:{
         type:String,
         required:true,
-        unique:true
     },
     email:{
         type:String,
         required:true,
-        unique:true
     },
     password:{
         type:String,
-        required:true,
     },
  },{timestamps:true})
  const Admin = mongoose.model('Admin', AdminSchema);
